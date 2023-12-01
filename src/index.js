5a110032c36e5c74d8a3bc5fc4584d989f6486f4
AndreaDiaz23 <andrea.grafico23@gmail.com>
2023-11-30 20:59:15 -0500

    addinfo

--- src/index.js
+++ src/index.js
@@ -1,62 +1,81 @@
-import axios from "axios";
-axios.defaults.headers.common["x-api-key"] = live_MLktqQ19jStPH3IF1EIfPNYJPjvXVQbD17osTKpGLLDOB33gmte96Cp1XRiEtyyC;
+import axios from 'axios';
 
-
-const breedSelect = document.querySelector ('.breed-select');
+const breedSelect = document.querySelector('.breed-select');
 const load = document.querySelector('.loader');
 const fail = document.querySelector('.error');
 const infoCat = document.querySelector('.cat-info');
 
+fail.style.display = 'none';
+load.style.display = 'none';
 
-function fetchBreeds() {
-    fetch(
-        'https://api.thecatapi.com/v1/breeds?api_key=live_MLktqQ19jStPH3IF1EIfPNYJPjvXVQbD17osTKpGLLDOB33gmte96Cp1XRiEtyyC'
-    ).then(response =>{
-        response
-        .json()
-        .then(data => {
-            data.map(breed =>{ 
-                breedSelect.innerHTML += `<breedSelect value=${breed.reference_image_id}>${brees.name}</breedSelect>`;
-            });
-            console.log(data);
+const fetchBreeds = async () => {
+  const response = await fetch(
+    'https://api.thecatapi.com/v1/breeds?api_key=live_MLktqQ19jStPH3IF1EIfPNYJPjvXVQbD17osTKpGLLDOB33gmte96Cp1XRiEtyyC'
+  );
+  console.log(response);
 
-            return data;
-        })
-        .catch(error =>{
-            infoCat.innerHTML = `<p class="error"> Error: ${error}</p>`;
-            console.log(error);
-        });
-    });   
-}
+  const body = await response.json();
+  console.log(body);
 
-const dataApi = fetchBreeds();
+  body.forEach(element => {
+    udpateList(element.id, element.name);
+  });
+};
 
-breedSelect.addEventListener('change', event =>{
-    fetchImage(event.target.value)
-    .then(response => {
-        load.style.display = 'none';
-        response.json().then(data => {
-            console.log(data);
+fetchBreeds();
 
-            infoCat.innerHTML = `
-            <img class="cat-info__cat-image" src=${data.url} alt="" />
-            <div class="cat-description">
-                <h2 class="cat-description__cat-breed> ${data.breeds[0].name} </h2>
-                <p class="cat-description__cat-temperament"> ${data.breeds[0].description} </p>
-                <h3 class="cat-description__cat-temperament"> Temperament </h3>
-                <p class="cat-description__cat-temperament"> ${data.breeds[0].temperament} </p>
-            </div>`;
-        });
-    })
-    .catch(error => {
-        console.log(error);
-    });
+const udpateList = (id, nameCat) => {
+  const option = document.createElement('option');
+  option.value = id;
+  option.textContent = nameCat;
+  breedSelect.append(option);
+};
+
+breedSelect.addEventListener('change', async event => {
+  try {
+    const response = await fetchCatByBreed(event.target.value);
+  } catch (error) {
+    fail.style.display = 'block';
+    console.log(error);
+  }
 });
 
-function fetchImage(id) {
-    load.style.display = 'inline-block';
-    fail.style.display = 'none';
-    infoCat.innerHTML ='';
+const fetchCatByBreed = async breedId => {
+  const response = await fetch(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+  );
+  const data = await response.json();
+  console.log(data);
 
-    return fetch(`https://api.thecatapi.com/v1/images/${id}`);
+  const newFetch = await fetch(
+    `https://api.thecatapi.com/v1/images/${data[0].id}`
+  );
+
+  const newData = await newFetch.json();
+  console.log(newData);
+
+  const nameCat = newData.breeds[0].name;
+  console.log(nameCat);
+
+  const catDesc = newData.breeds[0].description;
+  console.log(catDesc);
+
+  const catTemp = newData.breeds[0].temperament;
+  console.log(catTemp);
+
+  const catUrl = newData.url;
+  console.log(catUrl);
+
+  createCard(nameCat, catDesc, catUrl, catTemp);
+};
+
+function createCard(nameCat, description, url, temperament) {
+  infoCat.innerHTML = `
+            <img class="cat-info__cat-image" src=${url} alt="" />
+            <div class="cat-description">
+                <h2 class="cat-description__cat-breed> ${nameCat} </h2>
+                <p class="cat-description__cat-temperament"> ${description} </p>
+                <h3 class="cat-description__cat-temperament"> Temperament </h3>
+                <p class="cat-description__cat-temperament"> ${temperament} </p>
+            </div>`;
 }
